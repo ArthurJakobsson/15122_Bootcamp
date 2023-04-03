906b169fccc9fde3e0dfec3d0eec4a7de36c388c
ArthurJakobsson <arthur.k.jakobsson@gmail.com>
2023-04-03 12:00:53 -0400

    added ex5, small changes

---
+++ s23_pilot_bootcamp/code/ex5-correct.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <assert.h>
+
+/**
+ * @file ex5-bad.c
+ * @brief Example (5) demonstrating contracts and test cases (TA example)
+ *
+ * 15-122: Principles of Imperative Computation
+ * Spring 2023 - Debugging in C Pilot Bootcamp
+ *
+ *
+ * This is a Correct implementation.
+ *
+ * @author Arthur Jakobsson <ajakobss@andrew.cmu.edu>
+ * @author Liz Chu <echu2@andrew.cmu.edu>
+ */
+
+
+typedef int pixel; //NOTE the lack of a star (not a pointer)
+
+int new_green(pixel pix)
+{
+
+}
+
+int main()
+{
+  //implement a pixel with garbled bytes
+  //phrase it as if we have fully running code but we don't pass autolab ;-;
+}
